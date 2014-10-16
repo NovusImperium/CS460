b5794095fbbce24ad818091f263ab5c84270f41d
Amandeep Gill <gilamand@seawolf.sonoma.edu>
2014-10-16 16:58:02 -0700

    more changes?

--- proj1/Project1.c
+++ proj1/Project1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "SetLimits.h"
 #include "lex.h"
 
+<<<<<<< Updated upstream
 int main(int argc, char *argv[]) {
     lexical_t t;
     if (argc < 2) {
@@ -10,6 +12,19 @@ int main(int argc, char *argv[]) {
     }
     init_lex(argv[1]);
     while ((t = get_token()) != eof_tok) {
+=======
+int main (int argc, char * argv[])
+{
+    SetLimits();
+	lexical_t t;
+	if (argc < 2)
+	{
+		printf ("format: proj1 <filename>\n");
+		exit (1);
+	}
+	init_lex (argv[1]);
+	while ((t = get_token()) != eof_tok) {
+>>>>>>> Stashed changes
         printf("\t%-12s%s\n", token_names[t], get_lexeme());
     }
     end_lex();
