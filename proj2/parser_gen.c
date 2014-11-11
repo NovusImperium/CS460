a957623e12a0c27bfc2d6d8a90920999e957db78
Amandeep Gill <gilamand@seawolf.sonoma.edu>
2014-11-10 22:06:38 -0800

    ready for reading in and parsing rules

--- proj2/parser_gen.c
+++ proj2/parser_gen.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "parser_gen.h"
 #include "array.h"
+#include "set.h"
 
 static FILE *rules;
 static char *fstring;
 static size_t fstr_len = 0;
+static size_t f_curr = 0;
 static FILE *parser;
 
 static array *terminals;
@@ -12,16 +14,24 @@ static int t_len = 0;
 static array *nonterminals;
 static int nt_len = 0;
 
+static char *output;
+
 typedef struct {
     char *nterm;
-    array *firsts;
-    array *follows;
+    set *firsts;
+    set *follows;
     array *rules;
 } prod_rules;
 static array *productions;
 
 // reads in the terminal and nonterminals symbols
-inline void read_syms();
+inline bool read_syms();
+
+// read in the production rules
+inline bool read_rules();
+
+// convert the production rules from c string to ints
+inline bool convert_rules();
 
 inline bool generate(char *in, char *out) {
     if ((rules = fopen(in, "r")) == 0 || getdelim(&fstring, &fstr_len, EOF, rules) <= 0) {
@@ -35,4 +45,19 @@ inline bool generate(char *in, char *out) {
     }
 
     return true;
+}
+
+inline bool read_syms() {
+
+    return false;
+}
+
+inline bool read_rules() {
+
+    return false;
+}
+
+inline bool convert_rules() {
+
+    return false;
 }
