2b4321600d53359320b6a9551cd0d41b70c646a5
Amandeep Gill <gilamand@seawolf.sonoma.edu>
2014-11-22 16:49:29 -0800

    done with symbol table, except for write_syms

--- proj3/sym.c
+++ proj3/sym.c
@@ -1,4 +1,8 @@
 #include "sym.h"
+#include "hashmap.h"
+
+#include <string.h>
+#include <malloc.h>
 
 struct sym {
     char *id;
@@ -9,24 +13,57 @@ struct sym {
     };
 };
 
-optional get_sym(sym_tab *t, char *id) {
+// hash function for char*
+static unsigned hash(void *);
+
+// equality comparison function for char*
+static bool cmp(void *, void *);
+
+inline optional init_sym() {
+    return hashmap_init(1024, hash, cmp);
+}
+
+inline optional get_sym(table *t, char *id) {
+    return hashmap_get(t, id);
+}
+
+inline bool insert_sym(table *t, char *id, optional val) {
+    sym *s = malloc(sizeof(sym));
+    s->id = id;
+    if ((s->flag = val.e)) {
+        s->ival = *(long long *) val.val;
+    } else {
+        s->dval = *(double *) val.val;
+    }
+    return hashmap_insert(t, s, id);
+}
+
+inline optional get_value(sym *s) {
     optional opt;
+    opt.val = (opt.e = s->flag) ? &s->ival : &s->dval;
     return opt;
 }
 
-bool insert_sym(sym_tab *t, char *id, optional val) {
-    return 0;
+inline char *get_id(sym *s) {
+    return s->id;
 }
 
-optional get_value(sym *s) {
-    optional opt;
-    return opt;
+inline void write_syms(table *t, FILE *out) {
+
 }
 
-char *get_id(sym *s) {
-    return null;
+static unsigned hash(void *a) {
+    char *str = (char *) a;
+    unsigned h = 0;
+
+    int c;
+    while ((c = *str++)) {
+        h = ((h << 5) + h) ^ c;
+    }
+
+    return h;
 }
 
-void write_syms(sym_tab *t, FILE *out) {
-
+static bool cmp(void *a, void *b) {
+    return strcmp((char *) a, (char *) b) == 0;
 }
--- proj3/sym.h
+++ proj3/sym.h
@@ -2,28 +2,33 @@
 #include "defs.h"
 #include "array.h"
 
-typedef struct sym_tab sym_tab;
+typedef struct hashmap table;
 typedef struct sym sym;
+
+// creates and initializes the symbol table
+// returns an optional such that
+//  if opt.e == true => opt.val is valid
+extern inline optional init_sym();
 
 // get a symbol entry from the symbol table
 // returns an optional such that:
 //  opt.e == true iff the symbol has a valid entry in the table
-inline optional get_sym(sym_tab *t, char* id);
+extern inline optional get_sym(table *t, char* id);
 
 // insert a symbol into the symbol table
-// val.e == true => val.val == integer
-// val.e == false => val.val == double
+// val.e == true => *val.val == integer
+// val.e == false => *val.val == double
 // returns true iff the symbol already exists
-inline bool insert_sym(sym_tab *t, char* id, optional val);
+extern inline bool insert_sym(table *t, char* id, optional val);
 
 // get the value of the symbol entry
 // returns an optional such that:
-//  opt.e == true => opt.val == integer
-//  opt.e == false => opt.val == double
-inline optional get_value(sym *s);
+//  opt.e == true => *opt.val == integer
+//  opt.e == false => *opt.val == double
+extern inline optional get_value(sym *s);
 
 // get the character string that represents the id of the symbol entry
-inline char *get_id(sym *s);
+extern inline char *get_id(sym *s);
 
 // writes the symbol entries in alphabetical order to the given file pointer
-inline void write_syms(sym_tab *t, FILE *out);
+extern inline void write_syms(table *t, FILE *out);
